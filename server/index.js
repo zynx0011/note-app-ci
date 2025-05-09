45669225b2e5082450e7c04661fe3b33fe98e437
zynx0011 <zrdrc31@gmail.com>
2025-05-09 10:31:16 +0000

    testing

--- server/index.js
+++ server/index.js
@@ -31,7 +31,7 @@ try {
 }
 
 app.get("/", (req, res) => {
-  res.send("Server Is Running Successfully With Express JS");
+  res.send("Server Is Running Successfully With Express JS Welcome");
 });
 
 app.use("/auth", authRoutes);
